1ead0c704b7d8e304437c5063829a1498e72940d
apollodevv <mxrcdev@gmail.com>
2021-05-28 18:55:15 +0200

    feat: WebSocket

---
+++ src/index.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+    Client: require('./client/Client'),
+
+    Constants: require('./util/Constants'),
+    Intents: require('./util/Intents'),
+
+    WebSocket: require('./client/websocket/WebSocket')
+}
