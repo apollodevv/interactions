04b26dba20d1a7dabbfd6338767a9f460f4f8756
apollodevv <mxrcdev@gmail.com>
2021-06-01 20:31:01 +0200

    Errors: add messages

---
+++ src/errors/Messages.js
@@ -0,0 +1,10 @@
+"use strict";
+
+exports.Errors = {
+    MISSING_TOKEN: 'No token was provided',
+    INVALID_INTENTS: 'Invalid intents were provided to ClientOptions',
+
+    REQUEST_ABORTED: 'Request was aborted',
+
+    WS_ERROR: 'WebSocket: An error occurred'
+}
